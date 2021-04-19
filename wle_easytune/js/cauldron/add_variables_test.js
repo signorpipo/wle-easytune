fe1abf17e7775702cc8e27979de979ebea944806
Elia Ducceschi <eliaducceschi@hotmail.it>
2021-04-19 21:25:36 +0200

    Fix name

--- wle_easytune/js/cauldron/add_variables_test.js
+++ wle_easytune/js/cauldron/add_variables_test.js
@@ -1,4 +1,4 @@
-WL.registerComponent('addVariables', {
+WL.registerComponent('add-variables', {
 }, {
     init: function () {
         PP.EasyTuneVariables.addVariable(new PP.EasyTuneNumber("Speed", 10.32, 0.01, 3));
